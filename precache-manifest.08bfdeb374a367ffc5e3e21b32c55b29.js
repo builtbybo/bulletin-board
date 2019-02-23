8209c7b3e9e2503944b4cf02cdbff59c36939919
B A-W <lanlecreates@gmail.com>
2019-02-23 22:49:09 +0000

    Updates

---
+++ precache-manifest.08bfdeb374a367ffc5e3e21b32c55b29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fa3e4f5dbc9a04a4603",
+    "url": "/bulletin-board/static/js/runtime~main.8fa3e4f5.js"
+  },
+  {
+    "revision": "42cb225691db2826b070",
+    "url": "/bulletin-board/static/js/main.42cb2256.chunk.js"
+  },
+  {
+    "revision": "28ad1660c4f4a997b44a",
+    "url": "/bulletin-board/static/js/2.28ad1660.chunk.js"
+  },
+  {
+    "revision": "42cb225691db2826b070",
+    "url": "/bulletin-board/static/css/main.13ea9599.chunk.css"
+  },
+  {
+    "revision": "d43e23e68127510ad6ee7e4bcf5da136",
+    "url": "/bulletin-board/index.html"
+  }
+];
